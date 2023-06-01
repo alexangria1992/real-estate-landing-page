73a005aa3f01e25e311c186d74f5bd72c66ba76e
alexangria1992 <alex040892@gmail.com>
2023-06-01 15:55:20 +1200

    desktop and mobile header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,12 +6,12 @@ import Logo from "../assets/img/logo.svg";
 
 const Header = () => {
   return (
-    <header className="py-6 mb-12 border-2 border-red-500 border-b">
-      <div className="container mx-auto border-2 border-green-500 flex justify-between items-center">
+    <header className="py-6 mb-12  border-b">
+      <div className="container mx-auto  flex justify-between items-center">
         <Link to="/">
           <img src={Logo} alt="" />
         </Link>
-        <div className="flex items-center gap-6 border-blue-300 border-2 ">
+        <div className="flex items-center gap-6  ">
           <Link to="" className="hover:text-violet-900 transition">
             Log in
           </Link>
