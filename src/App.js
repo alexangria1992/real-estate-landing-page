e43a4cef2e88b4d1e7a4ec0fef4c4fa2e15af5be
alexangria1992 <alex040892@gmail.com>
2023-06-01 15:28:18 +1200

    desktop header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-function App() {
-  return <div className="App"></div>;
-}
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+// import components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+// import pages
+import Home from "./pages/Home";
+import PropertyDetails from "./pages/PropertyDetails";
+
+const App = () => {
+  return (
+    <div className="max-w-[1440px] bg-white mx-auto ">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/property/:id" element={<PropertyDetails />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,30 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
+import Logo from "../assets/img/logo.svg";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="py-6 mb-12 border-2 border-red-500 border-b">
+      <div className="container mx-auto border-2 border-green-500 flex justify-between items-center">
+        <Link to="/">
+          <img src={Logo} alt="" />
+        </Link>
+        <div className="flex items-center gap-6 border-blue-300 border-2 ">
+          <Link to="" className="hover:text-violet-900 transition">
+            Log in
+          </Link>
+          <Link
+            to=""
+            className="bg-violet-700 hover:bg-violet-800 text-white px-4 py-3 rounded-lg transition"
+          >
+            Sign Up
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
